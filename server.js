1c48cef237295fb716f192f8561fab189c45c09a
toam <toamcohen.transformation@gmail.com>
2020-12-06 16:15:13 +0200

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
-mongoose.connect("mongodb://localhost/weatherAppFixDB", { useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/weatherAppFixDB", { useUnifiedTopology: true });
 const api = require("./server/routers/api");
 
 app.use(express.json());
@@ -13,5 +13,5 @@ app.use(express.static(path.join(__dirname, "node_modules")));
 
 app.use("/", api);
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("server is up and runing"));
